48af678be36d98c3d6327fded215b1d272b9248a
André Rodrigues <andre.rodrigues.he@gmail.com>
2022-04-16 19:56:27 -0300

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
-const client_id = '8f7f9ea0c57a47ecb930b57d4c87be77'
-const client_secret = '083c8ff279554e7eb319c53db9e0cb86'
+const client_id = 
+const client_secret = 
 
 export { client_id, client_secret }
